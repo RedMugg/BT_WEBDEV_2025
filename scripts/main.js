33c2d2efda44b0e844701b682255a7753527b0b3
Stephan Kok <stephan.kok.345@gmail.com>
2025-03-21 10:44:43 +0100

    First try at LocalStorage + Accent-color

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,47 @@
-function toggleRadio() {
-    const fieldsetOneTwo = document.getElementById("fieldsetOneTwo");
-    const radioTest = document.getElementsByName("getrouwd");
+// document.getElementsByName("getrouwd").addEventListener("change", function () {
+//     toggleRadio();
+// });
 
-    if (radioTest.value = "j") {
-        fieldsetOneTwo.toggleAttribute('disabled');
+// function toggleRadio() {
+//     console.log(radioButton.value);
+//     const fieldsetOneTwo = document.getElementById("fieldsetOneTwo");
 
-    } else if (radioTest.value = "n") {
-        fieldsetOneTwo.toggleAttribute('disabled');
+//     fieldsetOneTwo.toggleAttribute('disabled');
+// }
+
+// Local Storage
+window.onload = function () {
+
+    if (storageAvailable("localStorage")) {
+        const voorLettersInput = document.getElementById("voorLetters");
+        voorLettersInput.addEventListener("change", function() {
+            localStorage.setItem(this.id, document.getElementById("voorLetters").value);
+
+        })
+        
+
+        let voorLettersVal = localStorage.getItem("voorLetters");
+        voorLettersInput.value = voorLettersVal;
+    } else {
+        // Too bad, no localStorage for us
     }
+}
 
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
 }
